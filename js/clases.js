8b8802631b7cbe71487bcefbb9d892249f49477c
Cristhiam A Vasquez H <cristhiam.vasquez@gmail.com>
2018-08-24 12:51:35 -0300

    first commit

---
+++ js/clases.js
@@ -0,0 +1,8 @@
+/*============================================
+CLASES
+las clases, son funciones constructoras y siempre se inicializan con Mayuscula
+ejemplo: new String()
+==============================================*/
+/*============================================
+CLASES PRIMITIVAS
+==============================================*/
